57b5d08527f421f0e36840aa7cb00ab00f23409f
Heather Park <hthr.prk@gmail.com>
2016-07-08 18:29:04 -0700

    Implement necessary controllers

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,16 @@
 angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
-  // Your code here
+
+  $scope.data = {};
+
+  $scope.getAll = function () {
+    Links.getAll()
+    .then(function (data) {
+      $scope.data.links = data;
+    });
+  };
+  return $scope.getAll();
 });
+
+
--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,7 +1,24 @@
 angular.module('shortly.services', [])
 
 .factory('Links', function ($http) {
-  // Your code here
+
+  return {
+    getAll : function () {
+      return $http({method: 'GET',
+        url: '/api/links'
+      })
+      .then(function (resp) {
+        return resp.data;
+      });
+    },
+
+    addOne: function (data) {
+      return $http({method: 'POST',
+        url: '/api/links',
+        data: data
+      });
+    }
+  };
 })
 .factory('Auth', function ($http, $location, $window) {
   // Don't touch this Auth service!!!
